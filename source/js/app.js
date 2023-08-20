b2fbfdd9b43fae8c102f8b1ebb67a6809c570647
Danil Gurin <gooreen.frontend@gmail.com>
2023-08-20 16:55:52 +0500

    adaptive

---
+++ source/js/app.js
@@ -0,0 +1,17 @@
+const navElement = document.querySelector('.page-header__nav');
+const buttonElement = document.querySelector('.page-header__toggler');
+
+
+// Menu-burger
+
+navElement.classList.add('page-header__nav--closed');
+
+buttonElement.addEventListener('click', () => {
+  if (navElement.classList.contains('page-header__nav--closed')) {
+    navElement.classList.add('page-header__nav--opened');
+    navElement.classList.remove('page-header__nav--closed');
+  } else {
+    navElement.classList.add('page-header__nav--closed');
+    navElement.classList.remove('page-header__nav--opened');
+  }
+});
